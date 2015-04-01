dfe5a035c763cf513eb09286957d2861b9861976
cbuu <ioricbuu@gmail.com>
2015-04-01 10:17:20 +0800

    add some comment

--- Classes/Demo/Demo.cpp
+++ Classes/Demo/Demo.cpp
@@ -30,30 +30,38 @@ bool Demo::init()
 }
 
 void Demo::testVector(){
+	//创建Vector对象
 	Vector<Sprite*> container;
 
+	//创建三个精灵
 	Sprite* A = Sprite::create();
 	Sprite* B = Sprite::create();
 	Sprite* C = Sprite::create();
 
+	//为每个精灵上标签
 	A->setTag(0);
 	B->setTag(1);
 	C->setTag(2);
 
+	//从后插入对象
 	container.pushBack(A);
 	container.pushBack(B);
 	container.pushBack(C);
 
+	//获取数组的最后一个对象
 	Sprite* sp = container.back();
 	log("the tag is %d", sp->getTag());
 
+	//获取数组的第二个对象
 	sp = container.at(1);
 	log("the tag is %d", sp->getTag());
 
+	//利用迭代器进行Vector的遍历
 	Vector<Sprite*>::iterator it = container.begin();
 
 	for (; it != container.end();)
 	{
+		//定向删除标签为1的精灵
 		if ((*it)->getTag() == 1)
 		{
 			it = container.erase(it);
@@ -73,6 +81,7 @@ void Demo::testTMX()
 {
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 
+	//创建tiledmap对象
 	TMXTiledMap* tmx = TMXTiledMap::create("PushBox/map.tmx");
 	tmx->setPosition(visibleSize.width / 2, visibleSize.height / 2);
 	tmx->setAnchorPoint(Vec2(0.5, 0.5));
@@ -81,15 +90,18 @@ void Demo::testTMX()
 
 void Demo::testUserDefault()
 {
+	//判断是否已有本地数据文件，非必须
 	if (!UserDefault::getInstance()->getBoolForKey("isExist"))
 	{
 		UserDefault::getInstance()->setBoolForKey("isExist", true);
 	}
 
+	//以字符串形式存储
 	UserDefault::getInstance()->setStringForKey("key", "value");
 	string word = UserDefault::getInstance()->getStringForKey("key");
 	log("the word is %s " , word);
 
+	//以整形形式存储
 	UserDefault::getInstance()->setIntegerForKey("value", 14);
 	int value = UserDefault::getInstance()->getIntegerForKey("value");
 	log("the value is %d ", value);
@@ -97,17 +109,21 @@ void Demo::testUserDefault()
 
 void Demo::testSQLite()
 {
+	//数据库指针
 	sqlite3* pdb = NULL;
 	
+	//数据库的路径
 	string path = FileUtils::getInstance()->getWritablePath() + "save.db";
 
+	//创建或打开数据库
 	int result = sqlite3_open(path.c_str(), &pdb);
 
 	if (result==SQLITE_OK)
 	{
 		log("database init");
 	}
-
+	
+	//创建一个新表
 	string sql = "create table hero(ID int primary key not null,name char(10));";
 
 	result = sqlite3_exec(pdb, sql.c_str(), NULL, NULL, NULL);
@@ -156,11 +172,13 @@ void Demo::testSQLite()
 	//务必释放指针
 	sqlite3_free_table(re);
 
+	//务必关闭数据库，否则会造成内存泄漏
 	sqlite3_close(pdb);
 }
 
 void Demo::testMap()
 {
+	//创建Map对象
 	Map<string, Sprite*> map;
 
 	Sprite* A = Sprite::create();
@@ -171,18 +189,21 @@ void Demo::testMap()
 	B->setTag(1);
 	C->setTag(2);
 
+	//键值对方式插入对象
 	map.insert("hero", A);
 	map.insert("monster", B);
 	map.insert("object", C);
 
+	//获取关键字为monster的对象
 	Sprite* sp = map.at("monster");
 	log("the tag is %d", sp->getTag());
 
+	//利用迭代器遍历Map
 	Map<string, Sprite*>::iterator it = map.begin();
 
 	for (; it != map.end();)
 	{
-		
+		//删除关键字为monster的对象，XXX.first为关键字，XXX.second为值
 		if ((*it).first == "monster")
 		{
 			it = map.erase(it);
